7092463697a08773cc205431cf8a81673931662f
Athos Gustavo <athosgustavowf@gmail.com>
2024-01-13 13:02:16 -0300

    Adição do input para escolha da foto do pet

--- lib/model/DataForm.dart
+++ lib/model/DataForm.dart
@@ -1,13 +1,16 @@
 
+import 'dart:io';
 
+import 'package:image_picker/image_picker.dart';
 class DataForm{
 
   String nome;
   String numero;
   String endereco;
   String nomePet;
+  File? fotoDoPet;
 
-  DataForm(this.nome, this.numero, this.endereco, this.nomePet);
+  DataForm({required this.nome, required this.numero, required this.endereco, required this.nomePet, required this.fotoDoPet});
 
 
 }
--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,13 +1,14 @@
 
 
+import 'dart:io';
+
 import 'package:find_pet/componentes/card_registro.dart';
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
 import '../controller/form_data_controller.dart';
 import '../model/DataForm.dart';
 
 class FormScreen extends StatefulWidget{
-  
   final FormDataController formDataController;
   FormScreen(this.formDataController);
 
@@ -17,11 +18,24 @@ class FormScreen extends StatefulWidget{
 }
 
 class _FormScreen extends State<FormScreen>{
-
   TextEditingController inputNomeController = TextEditingController();
   TextEditingController inputNumeroController = TextEditingController();
   TextEditingController inputEnderecoController = TextEditingController();
   TextEditingController inputNomePetController = TextEditingController();
+  File? imagemSelecionada;
+  
+  Future<void> adicionarImagemPet()async {
+    final imagePicker = ImagePicker();
+    final pickImage = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState((){
+      if(pickImage != null){
+        imagemSelecionada = File(pickImage.path);
+      }else{
+        //lança exceçao
+      }
+    });
+  }
   
   @override
   Widget build(BuildContext contexto){
@@ -49,9 +63,28 @@ class _FormScreen extends State<FormScreen>{
               controller: inputNomePetController,
               decoration: InputDecoration(labelText: 'Nome do pet'),
             ),
+            InkWell(
+              onTap:(){
+                adicionarImagemPet();
+              },
+              child: Ink(
+                width:double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  image: imagemSelecionada != null
+                    ? DecorationImage(
+                        image: FileImage(imagemSelecionada!),
+                        fit: BoxFit.cover,
+                      )
+                    : null,
+                    
+                  ),
+                ),
+            ),
             ElevatedButton(onPressed: (){
 
-              DataForm dataForm = DataForm(inputNomeController.text, inputNumeroController.text, inputEnderecoController.text, inputNomePetController.text);
+              DataForm dataForm = DataForm(nome:inputNomeController.text, numero:inputNumeroController.text, endereco:inputEnderecoController.text, nomePet:inputNomePetController.text, fotoDoPet:imagemSelecionada);
               widget.formDataController.adicionaPet(dataForm);  // widget é usado para se referir a propriedades que sao passadas de pai para filho
               Navigator.pop(context,  widget.formDataController);
 
