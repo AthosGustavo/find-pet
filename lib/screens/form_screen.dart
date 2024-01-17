dbfd6076ec36d38cc528a090b24507c23a08c459
Athos Gustavo <athosgustavowf@gmail.com>
2024-01-17 10:06:33 -0300

    Adicionando widget Form

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -63,83 +63,85 @@ class _FormScreen extends State<FormScreen>{
         title: Text('Registrar pet')
       ),
       body: Container(
-        child:ListView(
-          children: [
-            TextFormField(
-              controller: inputNomeController,
-              decoration: InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
-              controller: inputNumeroController,
-              decoration: InputDecoration(labelText: 'Número'),
-            ),
-            TextFormField(
-              controller: inputEnderecoController,
-              decoration: InputDecoration(labelText: 'Endereço'),
-            ),
-            TextFormField(
-              controller: inputNomePetController,
-              decoration: InputDecoration(labelText: 'Nome do pet'),
-            ),
-            TextFormField(
-              controller: inputEnderecoVistoPorUltimoController,
-              decoration: InputDecoration(labelText: 'Visto por último em...'),
-            ),
-            InkWell(
-              onTap:(){
-                adicionarImagemPet();
-              },
-              child: Ink(
-                width:double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  image: imgPet != null
-                    ? DecorationImage(
-                        image: FileImage(imgPet!),
-                        fit: BoxFit.cover,
-                      )
-                    : null,
-                    
+        child:Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: inputNomeController,
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                controller: inputNumeroController,
+                decoration: InputDecoration(labelText: 'Número'),
+              ),
+              TextFormField(
+                controller: inputEnderecoController,
+                decoration: InputDecoration(labelText: 'Endereço'),
+              ),
+              TextFormField(
+                controller: inputNomePetController,
+                decoration: InputDecoration(labelText: 'Nome do pet'),
+              ),
+              TextFormField(
+                controller: inputEnderecoVistoPorUltimoController,
+                decoration: InputDecoration(labelText: 'Visto por último em...'),
+              ),
+              InkWell(
+                onTap:(){
+                  adicionarImagemPet();
+                },
+                child: Ink(
+                  width:double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    image: imgPet != null
+                      ? DecorationImage(
+                          image: FileImage(imgPet!),
+                          fit: BoxFit.cover,
+                        )
+                      : null,
+                      
+                    ),
                   ),
-                ),
-            ),
-            InkWell(
-              onTap:(){
-                adicionarImgVistoPorUltimo();
-              },
-              child: Ink(
-                width:double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  image: imgUltimoLugarVisto != null
-                    ? DecorationImage(
-                        image: FileImage(imgUltimoLugarVisto!),
-                        fit: BoxFit.cover,
-                      )
-                    : null,
-                    
+              ),
+              InkWell(
+                onTap:(){
+                  adicionarImgVistoPorUltimo();
+                },
+                child: Ink(
+                  width:double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    image: imgUltimoLugarVisto != null
+                      ? DecorationImage(
+                          image: FileImage(imgUltimoLugarVisto!),
+                          fit: BoxFit.cover,
+                        )
+                      : null,
+                      
+                    ),
                   ),
-                ),
-            ),
-            ElevatedButton(onPressed: (){
+              ),
+              ElevatedButton(onPressed: (){
+                
+                if(imgUltimoLugarVisto != null){
+                  DataForm dataForm = DataForm(nome:inputNomeController.text, numero:inputNumeroController.text, endereco:inputEnderecoController.text, nomePet:inputNomePetController.text, enderecoVistoPorUltimo: inputEnderecoVistoPorUltimoController.text, imgPet:imgPet, imgUltimoLugarVisto: imgUltimoLugarVisto);
+                  widget.formDataController.adicionaPet(dataForm);  // widget é usado para se referir a propriedades que sao passadas de pai para filho
+          
+                }else{
+                  DataForm dataForm = DataForm(nome:inputNomeController.text, numero:inputNumeroController.text, endereco:inputEnderecoController.text, nomePet:inputNomePetController.text, enderecoVistoPorUltimo: inputEnderecoVistoPorUltimoController.text, imgPet:imgPet);
+                  widget.formDataController.adicionaPet(dataForm);
+                }
+                Navigator.pop(context,  widget.formDataController);
+          
+                }, 
+                child: Text('Registrar')
+              )
               
-              if(imgUltimoLugarVisto != null){
-                DataForm dataForm = DataForm(nome:inputNomeController.text, numero:inputNumeroController.text, endereco:inputEnderecoController.text, nomePet:inputNomePetController.text, enderecoVistoPorUltimo: inputEnderecoVistoPorUltimoController.text, imgPet:imgPet, imgUltimoLugarVisto: imgUltimoLugarVisto);
-                widget.formDataController.adicionaPet(dataForm);  // widget é usado para se referir a propriedades que sao passadas de pai para filho
-
-              }else{
-                DataForm dataForm = DataForm(nome:inputNomeController.text, numero:inputNumeroController.text, endereco:inputEnderecoController.text, nomePet:inputNomePetController.text, enderecoVistoPorUltimo: inputEnderecoVistoPorUltimoController.text, imgPet:imgPet);
-                widget.formDataController.adicionaPet(dataForm);
-              }
-              Navigator.pop(context,  widget.formDataController);
-
-              }, 
-              child: Text('Registrar')
-            )
-            
-          ]
+            ]
+          ),
         )
       ),
     );
