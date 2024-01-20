cb7f828cc8c52d7aa44719f1c4048418c2c95315
Athos Gustavo <athosgustavowf@gmail.com>
2024-01-19 22:22:28 -0300

    personalização card mvp

--- lib/componentes/card_registro.dart
+++ lib/componentes/card_registro.dart
@@ -19,74 +19,132 @@ class CardRegistro extends StatefulWidget {
     this.nomePet = DataForm.nomePet;
     this.enderecoVistoPorUltimo = DataForm.enderecoVistoPorUltimo;
     this.imgPet = DataForm.imgPet;
-    
-    if(DataForm.imgUltimoLugarVisto != null){
-      this.imgUltimoLugarVisto = DataForm.imgUltimoLugarVisto;  
+
+    if (DataForm.imgUltimoLugarVisto != null) {
+      this.imgUltimoLugarVisto = DataForm.imgUltimoLugarVisto;
     }
-    
   }
 
   @override
-  _CardRegistro createState() => _CardRegistro();
+  _CardRegistroState createState() => _CardRegistroState();
 }
 
-class _CardRegistro extends State<CardRegistro> {
+class _CardRegistroState extends State<CardRegistro> {
   @override
   Widget build(BuildContext context) {
-    return Stack(alignment: Alignment.bottomCenter, children: [
-      InkWell(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
                   builder: (context) => DetailsScreen(
-                      nome: widget.nome,
-                      numero: widget.numero,
-                      endereco: widget.endereco,
-                      nomePet: widget.nomePet,
-                      enderecoVistoPorUltimo: widget.enderecoVistoPorUltimo,
-                      imgPet: widget.imgPet,
-                      imgUltimoLugarVisto: widget.imgUltimoLugarVisto,
-
-
+                    nome: widget.nome,
+                    numero: widget.numero,
+                    endereco: widget.endereco,
+                    nomePet: widget.nomePet,
+                    enderecoVistoPorUltimo: widget.enderecoVistoPorUltimo,
+                    imgPet: widget.imgPet,
+                    imgUltimoLugarVisto: widget.imgUltimoLugarVisto,
+                  ),
+                ),
+              );
+            },
+            child: Ink(
+              height: 120,
+              width: 350,
+              decoration: BoxDecoration(
+                color: Colors.grey[50],
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0,2),
                   )
-              )
-          );
-        },
-        child: Ink(
-          height: 220,
-          width: 280,
-          padding:
-              EdgeInsets.only(top: 8.0, right: 16.0, bottom: 8.0, left: 16.0),
-          // /margin: EdgeInsets.all(8.0), // Adiciona uma margem externa
-          decoration: BoxDecoration(
-            color: Colors.blue, // Cor de fundo azul
-            borderRadius: BorderRadius.circular(10.0), // Borda arredondada
+                ]
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(right: 40),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 5,
+                        ),
+                        color: Colors.grey[200],
+                        image: widget.imgPet != null
+                            ? DecorationImage(
+                                image: FileImage(widget.imgPet!),
+                                fit: BoxFit.cover,
+                              )
+                            : null,
+                      ),
+                      
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    
+                    children: [
+                      
+                      Padding(
+                        padding: const EdgeInsets.only(bottom:10.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          //mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.pets, // Adicione um ícone que lembre um pet aqui
+                              color: Colors.green,
+                              size: 20.0,
+                            ),
+                            Padding(
+                            padding: const EdgeInsets.only(left:5.0),
+                            child: Text(widget.nomePet),
+                          ),
+                          ],
+                        ),
+                      ),
+                      
+                      Row(
+                        //crossAxisAlignment: CrossAxisAlignment.center,
+                        //mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            
+                            Icons.location_on, // Adicione um ícone de localização aqui
+                            color: Colors.green,
+                            size: 20.0,
+                            
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left:5.0),
+                            child: Text(widget.enderecoVistoPorUltimo),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-          child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start, // Alinha o texto à esquerda
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-              Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    image: widget.imgPet != null
-                        ? DecorationImage(
-                            image: FileImage(widget.imgPet!),
-                            fit: BoxFit.cover,
-                          )
-                        : null,
-                  )),
-              Text(widget.nomePet),
-              Text(widget.enderecoVistoPorUltimo),
-            ],
-          ),
-        ),
+        ],
       ),
-    ]);
+    );
   }
 }
